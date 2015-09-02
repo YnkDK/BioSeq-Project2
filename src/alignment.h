e0655a5b75d6ef206179058b2874f8c5aa73ac53
Martin Storgaard <20140000@post.au.dk>
2015-09-02 10:01:25 +0200

    Added method to output latex code of alignment

--- src/alignment.h
+++ src/alignment.h
@@ -15,6 +15,7 @@
 #include <sys/types.h>
 #include <vector>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -23,6 +24,68 @@ public:
     virtual void compute_s(vector<vector<int64_t>> &cost, const vector<unsigned char> &seq1, const vector<unsigned char> &seq2) = 0;
     virtual void find_alignment(const vector<vector<int64_t>> &cost, vector<char> &alignment) = 0;
     virtual uint64_t num_alignments(vector<vector<int64_t>> &cost) = 0;
+
+    void print_alignment_latex(const vector<char> &alignment) {
+        vector<char> seq1;
+        vector<char> seq2;
+        seq1.reserve(50);
+        seq2.reserve(50);
+
+        cout << "\\begin{figure}[H]" << endl;
+        cout << "\t\\centering" << endl;
+        cout << "\t\\begin{tabular}{L{0.15\\textwidth} L{0.7\\textwidth}}" << endl;
+
+        for (int i = 0; i < alignment.size(); i += 2) {
+            seq1.push_back(alignment[i]);
+            seq2.push_back(alignment[i + 1]);
+            if (seq1.size() == 50) {
+                cout << "\t\tSequence 1 & \\texttt{";
+                for (int j = 0; j < seq1.size(); j++) {
+                    cout << seq1[j];
+                    if (j == 9 || j == 19 || j == 29 || j == 39) {
+                        cout << " ";
+                    }
+                }
+                cout << "} \\\\" << endl;
+
+                cout << "\t\tSequence 2 & \\texttt{";
+                for (int j = 0; j < seq2.size(); j++) {
+                    cout << seq2[j];
+                    if (j == 9 || j == 19 || j == 29 || j == 39) {
+                        cout << " ";
+                    }
+                }
+                cout << "} \\\\" << endl;
+                cout << "\t\t& \\\\" << endl;
+                seq1.clear();
+                seq1.reserve(50);
+                seq2.clear();
+                seq2.reserve(50);
+            }
+        }
+        cout << "\t\tSequence 1 & \\texttt{";
+        for (int j = 0; j < seq1.size(); j++) {
+            cout << seq1[j];
+            if (j == 9 || j == 19 || j == 29 || j == 39) {
+                cout << " ";
+            }
+        }
+        cout << "} \\\\" << endl;
+
+        cout << "\t\tSequence 2 & \\texttt{";
+        for (int j = 0; j < seq2.size(); j++) {
+            cout << seq2[j];
+            if (j == 9 || j == 19 || j == 29 || j == 39) {
+                cout << " ";
+            }
+        }
+        cout << "} \\\\" << endl;
+        cout << "\t\t& \\\\" << endl;
+        cout << "\t\\end{tabular}" << endl
+        << "\t\\caption{TODO: Make caption.}" << endl
+        << "\t\\label{tab:my_label}" << endl
+        << "\\end{figure}" << endl;
+    }
 
 private:
     /**
