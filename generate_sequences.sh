7a11b34e51d388da59831e730f28e533e4be9236
Martin Storgaard <20140000@post.au.dk>
2015-09-08 12:13:01 +0200

    Added first draft to statistics

---
+++ generate_sequences.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Define
+PROTEINS=('A' 'C' 'G' 'T')
+N=$1
+RANDOM=$2
+echo "> Random Sequence. Seed $2. Length: $1" > input/random-n$1-seed$2.fasta
+for i in $(seq 1 ${N});
+do
+    idx=$RANDOM;
+    echo -n ${PROTEINS[${idx} % 4]} >> input/random-n$1-seed$2.fasta
+done;
